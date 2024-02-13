7dba27dde140b3fbd9953f64e8b4d251e0d1e99e
RaftDev <Theraft08@gmail.com>
2024-02-13 01:03:02 +0100

    Builder Pattern Utility Annotations

---
+++ common/src/main/java/fr/atlasworld/common/annotation/OptionalBuilderArgument.java
@@ -0,0 +1,14 @@
+package fr.atlasworld.common.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Used to indicate that a builder argument is optional.
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.CLASS)
+public @interface OptionalBuilderArgument {
+}
